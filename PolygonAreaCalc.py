28c34db23db5369d64dd9e4b2254626de6c3bbfe
emilio <ermsharo@gmail.com>
2022-12-18 01:52:37 -0300

    update our changes

--- PolygonAreaCalc.py
+++ PolygonAreaCalc.py
@@ -3,10 +3,7 @@ import pandas as pd
 import ast
 
 df = pd.DataFrame()
-df = pd.read_csv("rand_simul.csv")
-
-
-# print(df["init_of_r_pos"][0])
+df = pd.read_csv("progress_simul.csv")
 
 
 def string_to_list(str_of_list):
@@ -42,34 +39,52 @@ def add_collumns_df():
     winner_points_density = []
 
     for row in df.itertuples():
-        s_points = string_to_list(row.init_of_s_pos)
-        r_points = string_to_list(row.init_of_r_pos)
-        p_points = string_to_list(row.init_of_p_pos)
-        w_points = string_to_list(row.winner_pos)
 
-        s_area = get_points_area(s_points)
-        r_area = get_points_area(r_points)
-        p_area = get_points_area(p_points)
-        win_area = get_points_area(w_points)
+        if row.count_of_r >= 4:
+            s_points = string_to_list(row.init_of_s_pos)
+            r_points = string_to_list(row.init_of_r_pos)
+            p_points = string_to_list(row.init_of_p_pos)
+            w_points = string_to_list(row.winner_pos)
 
-        s_points_area.append(s_area)
-        r_points_area.append(r_area)
-        p_points_area.append(p_area)
-        winner_points_area.append(win_area)
+            s_area = get_points_area(s_points)
+            r_area = get_points_area(r_points)
+            p_area = get_points_area(p_points)
+            win_area = get_points_area(w_points)
 
-        s_points_center.append(get_points_center(s_points))
-        r_points_center.append(get_points_center(r_points))
-        p_points_center.append(get_points_center(p_points))
-        winner_points_center.append(get_points_center(w_points))
+            s_points_area.append(s_area)
+            r_points_area.append(r_area)
+            p_points_area.append(p_area)
+            winner_points_area.append(win_area)
 
-        s_points_density.append(get_points_density(s_area, row.count_of_s))
-        r_points_density.append(get_points_density(r_area, row.count_of_r))
-        p_points_density.append(get_points_density(p_area, row.count_of_p))
-        winner_points_density.append(
-            get_points_density(
-                win_area, row.count_of_s + row.count_of_p + row.count_of_r
+            s_points_center.append(get_points_center(s_points))
+            r_points_center.append(get_points_center(r_points))
+            p_points_center.append(get_points_center(p_points))
+            winner_points_center.append(get_points_center(w_points))
+
+            s_points_density.append(get_points_density(s_area, row.count_of_s))
+            r_points_density.append(get_points_density(r_area, row.count_of_r))
+            p_points_density.append(get_points_density(p_area, row.count_of_p))
+            winner_points_density.append(
+                get_points_density(
+                    win_area, row.count_of_s + row.count_of_p + row.count_of_r
+                )
             )
-        )
+        else:
+
+            s_points_area.append("")
+            r_points_area.append("")
+            p_points_area.append("")
+            winner_points_area.append("")
+
+            s_points_center.append("")
+            r_points_center.append("")
+            p_points_center.append("")
+            winner_points_center.append("")
+
+            s_points_density.append("")
+            r_points_density.append("")
+            p_points_density.append("")
+            winner_points_density.append("")
 
     df["s_points_area"] = s_points_area
     df["r_points_area"] = r_points_area
@@ -88,23 +103,7 @@ def add_collumns_df():
 
     # print("df", df)
     df2 = df
-    df2.to_csv("rand_simul_new.csv", index=False)
+    df2.to_csv("progress_simul_new.csv", index=False)
 
 
 add_collumns_df()
-
-
-## Calcular a area entre os pontos
-# print(" S - area", get_points_area(df["init_of_s_pos"][0]))
-# print(" R - area", get_points_area(df["init_of_r_pos"][0]))
-# print(" P - area", get_points_area(df["init_of_p_pos"][0]))
-# print("centroid", get_points_center(df["init_of_p_pos"][0]))
-# print("densidade", get_points_density(df["init_of_p_pos"][0]))
-
-## Calcular a dispersão dos pontos
-
-
-## Calcular o ponto central dos pontos de cada elemento
-
-
-## Calcular o ponto central entre o ponto central dos elementos
